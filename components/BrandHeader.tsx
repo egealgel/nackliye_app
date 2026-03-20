845c77301bc21e0bb68c2d80dc50e77b9e4ad891
egealgel <egealgel@hotmail.com>
2026-03-20 19:48:54 +0300

    android text fix

--- components/BrandHeader.tsx
+++ components/BrandHeader.tsx
@@ -175,5 +175,7 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     letterSpacing: LETTER_SPACING,
     textAlign: 'center',
+    includeFontPadding: false,
+    textAlignVertical: 'center',
   },
 });
